080ebb841e0bd57c04106c5c35f3e4f1d698c5f7
kate_utkate <kate_utkate@vk.com>
2025-03-30 22:14:42 +0300

    add example role-based routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,22 +1,23 @@
 from flask import Blueprint, request, jsonify
 
 from app.auth.utils import generate_tokens, add_user_to_whitelist
+from app.middleware import role_required
 from app.models.user import User
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint("auth", __name__)
 
 
 # тестовый route для проверки работы redis
-@auth_bp.route('/ping', methods=['GET'])
+@auth_bp.route("/ping", methods=["GET"])
 def ping() -> dict:
     return {"message": "server is running"}
 
 
-@auth_bp.route('/login', methods=['POST'])
+@auth_bp.route("/login", methods=["POST"])
 def login() -> jsonify():
     data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
+    username = data.get("username")
+    password = data.get("password")
 
     user = User.find_by_username(username)
     if not user or user.password != password:
@@ -32,3 +33,15 @@ def login() -> jsonify():
             "role_number": user.role_number
         }
     )
+
+
+@auth_bp.route("/first-role", methods=["GET"])
+@role_required(1)
+def first_role_example() -> jsonify():
+    return jsonify({"message": "route available for first role"})
+
+
+@auth_bp.route("/second-role", methods=["GET"])
+@role_required(2)
+def second_role_example() -> jsonify():
+    return jsonify({"message": "route available for second role"})
