dda674da8fbd082c6f61161d14e20182c02f6aec
kate_utkate <kate_utkate@vk.com>
2025-03-30 20:35:55 +0300

    add user model

---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+
+@dataclass
+class User:
+    id: int
+    username: str
+    password: str
+    role_number: int
+
+    @staticmethod
+    def find_by_username(username: str) -> User | None:
+        if username == "testuser":
+            return User(1, "testuser", "password", 1)
+        return None
